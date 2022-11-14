ab86e734e66f1e8810573bfc251e8be9b6e47f5a
Joshua Kao <113385802+mrkaosha@users.noreply.github.com>
2022-11-14 12:26:37 -0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,94 @@
+namespace SpriteKind {
+    export const cursor = SpriteKind.create()
+    export const newCursor = SpriteKind.create()
+}
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    cursorGridRow += -1
+    cursorY += -10
+    drawGrid()
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    grid[cursorGridRow][cursorGridCol] = grid[cursorGridRow][cursorGridCol] * -1 + 1
+    drawGrid()
+})
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    cursorGridCol += -1
+    cursorX += -10
+    drawGrid()
+})
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    cursorGridCol += 1
+    cursorX += 10
+    drawGrid()
+})
+function drawGrid () {
+    sprites.destroyAllSpritesOfKind(SpriteKind.Player)
+    gridSprites = []
+    currentY = 0
+    for (let row of grid) {
+        currentX = 0
+        for (let gridSpace of row) {
+            if (gridSpace == 1) {
+                gridSprite = sprites.create(img`
+                    f f f f f f f f f f 
+                    f 7 7 7 7 7 7 7 7 f 
+                    f 7 7 7 7 7 7 7 7 f 
+                    f 7 7 7 7 7 7 7 7 f 
+                    f 7 7 7 7 7 7 7 7 f 
+                    f 7 7 7 7 7 7 7 7 f 
+                    f 7 7 7 7 7 7 7 7 f 
+                    f 7 7 7 7 7 7 7 7 f 
+                    f 7 7 7 7 7 7 7 7 f 
+                    f f f f f f f f f f 
+                    `, SpriteKind.Player)
+                gridSprite.left = currentX
+                gridSprite.top = currentY
+                gridSprites.push(gridSprite)
+            }
+            currentX += 10
+        }
+        currentY += 10
+    }
+    cursor.left = cursorX
+    cursor.top = cursorY
+}
+controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
+    cursorGridRow += 1
+    cursorY += 10
+    drawGrid()
+})
+let gridSprite: Sprite = null
+let currentX = 0
+let currentY = 0
+let gridSprites: Sprite[] = []
+let cursorY = 0
+let cursorX = 0
+let cursorGridRow = 0
+let cursorGridCol = 0
+let cursor: Sprite = null
+let grid: number[][] = []
+grid = []
+for (let row = 0; row <= 11; row++) {
+    grid.push([])
+    for (let column = 0; column <= 15; column++) {
+        grid[row].push(1)
+    }
+}
+cursor = sprites.create(img`
+    3 3 3 3 . . 3 3 3 3 
+    3 . . . . . . . . 3 
+    3 . . . . . . . . 3 
+    3 . . . . . . . . 3 
+    . . . . . . . . . . 
+    . . . . . . . . . . 
+    3 . . . . . . . . 3 
+    3 . . . . . . . . 3 
+    3 . . . . . . . . 3 
+    3 3 3 3 . . 3 3 3 3 
+    `, SpriteKind.newCursor)
+cursorGridCol = 0
+cursorGridRow = 0
+cursorX = 0
+cursorY = 0
+cursor.z = 10
+drawGrid()
